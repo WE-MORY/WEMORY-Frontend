09f9a7039c612d4452dcafcc5161391894e6ef27
minsgy <alstjrchl97@naver.com>
2021-04-29 02:21:18 +0900

    개발, 세팅::종속성 업데이트, Font 설정, Button 설정 완료

---
+++ frontend/src/assets/Colors/Color.js
@@ -0,0 +1,6 @@
+// 서비스 Color Components
+export const MAIN_COLOR = "#77A6FF"; // 핵심 컬러
+export const SIDE_MENU_COLOR = "#EDF3FF"; // 사이드 메뉴 패널 색깔
+export const SUB_COLOR = "#ABC8FF"; // 서브 컬러
+export const TEXT_BLACK = "#4B5364"; // 텍스트 블랙 컬러
+export const TEXT_WHITE = "#ffffff"; // 텍스트 화이트 컬러
