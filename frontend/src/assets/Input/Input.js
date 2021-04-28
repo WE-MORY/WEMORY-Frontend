e1f91441280af26d198bbe85c7b813dd1bab908a
qhahd78 <umuj0708@likelion.org>
2021-04-29 01:51:35 +0900

    세팅::Input

--- frontend/src/assets/Input/Input.js
+++ frontend/src/assets/Input/Input.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const InputStyle = styled.input`
+    width: 240px;
+    height: 40px;
+    border-radius: 30px;
+    border: 1px solid #77A6FF;
+    display: inline-block;
+    padding-left: 10px;
+    
+    &:focus{
+        outline-style: none;
+    }
+    `
 
 const Input = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <InputStyle placeholder="아이디를 입력하세요. "/>  
+        </>
     );
 }
 
