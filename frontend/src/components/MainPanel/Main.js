30e0a3d1668591f9ff84af0a60ad0a2faae4c95e
qhahd78 <umuj0708@likelion.org>
2021-04-29 17:58:42 +0900

    개발:: vector 추가

--- frontend/src/components/MainPanel/Main.js
+++ frontend/src/components/MainPanel/Main.js
@@ -2,10 +2,18 @@ import React from 'react';
 import Header from '../HeaderPanel/Header';
 import {ReactComponent as LightLogo} from '../../assets/Images/MainLogoLight2.svg';
 import styled from 'styled-components'
+import { MAIN_COLOR } from '../../assets/Colors/Color';
 
 const MainDiv = styled.div`
     display: flex;
+    flex-direction: column;
+    align-items: center;
     justify-content: center;
+`
+
+const Text = styled.div`
+    position: absolute;
+    
 `
 
 
@@ -15,7 +23,11 @@ const Main = () => {
             <Header />
 
             <MainDiv>
-            <LightLogo height="6vh" width="50vw" />
+                <LightLogo height="6vh" width="50vw" />
+                <svg width="250" height="250">
+                    <circle cx="125" cy="125" r="124" stroke={MAIN_COLOR} stroke-width="1" fill="none">안녕</circle>
+                </svg>
+                <Text>텍스트가 가운데에 들어갈까요 ?</Text>
             </MainDiv>
         </>
     );
