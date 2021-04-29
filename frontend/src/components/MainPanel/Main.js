3ef31363eddac20d25cc906365135f849f0999a3
qhahd78 <umuj0708@likelion.org>
2021-04-29 18:29:58 +0900

    개발::Main.js 레이아웃

--- frontend/src/components/MainPanel/Main.js
+++ frontend/src/components/MainPanel/Main.js
@@ -7,26 +7,44 @@ import { MAIN_COLOR } from '../../assets/Colors/Color';
 
 const MainDiv = styled.div`
     display: flex;
+    height: 60%;
     flex-direction: column;
     align-items: center;
-    justify-content: center;
+    justify-content: space-around;
 `
 
 const Text = styled.div`
     position: absolute;
+    text-align: center;
     
 `
 
+const CircleDiv = styled.div`
+    justify-content: center;
+    align-items: center;
+    display: flex;
+    height: 80vh;
+`
 
+const ListDiv = styled.div`
+    background-color: tomato;
+    height: 10vh;
+    width: 90vw;    
+` 
 const Main = () => {
     return (
         <>
             <Header />
 
             <MainDiv>
-                <LightLogo height="6vh" width="50vw" />
-                <Circle stroke = {MAIN_COLOR}/>
-                <Text>텍스트가 가운데에 들어갈까요 ?</Text>
+                <LightLogo height="10vh" width="60vw" />
+                <CircleDiv>
+                    <Circle stroke = {MAIN_COLOR}></Circle>
+                    <Text>일기를 작성할<br /> 계좌를 등록해주세요. </Text>
+                </CircleDiv>
+                <ListDiv>
+                    리스트 나올 div 입니다.
+                </ListDiv>
             </MainDiv>
         </>
     );
