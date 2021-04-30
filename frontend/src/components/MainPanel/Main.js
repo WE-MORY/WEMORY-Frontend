0af4a48e6c741b00cc8d779bee98bb1584d360d7
qhahd78 <umuj0708@likelion.org>
2021-05-01 02:52:52 +0900

    개발 :: main slider 준완

--- frontend/src/components/MainPanel/Main.js
+++ frontend/src/components/MainPanel/Main.js
@@ -8,60 +8,50 @@ import Button from '../../assets/Button/Button.js';
 import Pic1 from '../../assets/Images/cat.png';
 import Pic2 from '../../assets/Images/cat2.jpg';
 
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import SimpleSlider from './Test';
+
 const MainDiv = styled.div`
     display: flex;
     height: auto;
     flex-direction: column;
     align-items: center;
     margin-top: 2vh;
+    overflow-x: hidden;
 `
 
-const Text = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    position: absolute;
-    text-align: center;
-    height : 18vh;
-    font-size: 1.4rem;
+// const Text = styled.div`
+//     display: flex;
+//     flex-direction: column;
+//     justify-content: space-between;
+//     position: absolute;
+//     text-align: center;
+//     height : 18vh;
+//     font-size: 1.4rem;
     
-`
+// `
 
-const CircleDiv = styled.div`
-    /* position: absolute; */
-    justify-content: center;
-    align-items: center;
-    display: flex;
-    flex-direction: column;
-    height: 70vh;
-
-    svg{
-        stroke-dasharray: 100; /*이 값은 path의 길이와 동일합니다*/
-        stroke-dashoffset: 100;
-        animation: dash 1.3s linear;
-        animation-fill-mode: forwards;
-        
-        @keyframes dash {
-        0% {
-            stroke-dashoffset: 100;
-        }
-        100% {
-             stroke-dashoffset: 0;
-        }
-}
-    }
+// const CircleDiv = styled.div`
+//     /* position: absolute; */
+//     justify-content: center;
+//     align-items: center;
+//     display: flex;
+//     flex-direction: column;
+//     height: 70vh;
 
     
-`
+// `
 
-const BtnStyle = styled.div`
+// const BtnStyle = styled.div`
     
-    button {
-        max-width: 160px;
-        font-family: spoqa-Light;
+//     button {
+//         max-width: 160px;
+//         font-family: spoqa-Light;
 
-    }
-`
+//     }
+// `
 
 const ListDiv = styled.div`
     display: flex;
@@ -82,8 +72,28 @@ const TextBox =  styled.div`
     text-align: left;
     font-family: 'Spoca-Regular';
     font-size: 1.5rem;
-    margin: 2vh 0 ;
+    margin: 10vh 0 5vh 0 ;
     width: 85vw;
+`
+
+const Test = styled.div`
+    top: 25vh;
+    position: absolute;
+    svg{
+        stroke-dasharray: 100; /*이 값은 path의 길이와 동일합니다*/
+        stroke-dashoffset: 100;
+        animation: dash 1.3s linear;
+        animation-fill-mode: forwards;
+        
+        @keyframes dash {
+        0% {
+            stroke-dashoffset: 100;
+        }
+        100% {
+             stroke-dashoffset: 0;
+        }
+    }
+}
 `
 
 const Main = () => {
@@ -92,20 +102,13 @@ const Main = () => {
     return (
         <>
             <Header />
-
+            
             <MainDiv>
                 <LightLogo height="10vh" width="60vw" />
-                <CircleDiv>
+                <Test>
                     <Circle stroke = {MAIN_COLOR}></Circle>
-                    <Text>
-                        일기를 작성할<br />
-                        계좌를 등록해주세요.
-                        <BtnStyle>
-                            <Button Text={"등록하기"} Width={"50vw"}></Button>
-                        </BtnStyle>
-                    </Text>
-                    
-                </CircleDiv>
+                </Test>
+                <SimpleSlider />
                 <TextBox>
                     <span>
                         어쩌고 일기의 추억들
